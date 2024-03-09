dbee10c79f8f1f260dcb976edc291ea5250ce5dd
Palkov Alexey <palkov.alexey@gmail.com>
2024-03-09 11:44:24 +0300

    init

---
+++ src/main/java/com/example/crossdatabase/CrossDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.crossdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossDataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crossdatabase/CrossDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crossdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
